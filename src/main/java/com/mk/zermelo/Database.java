bd53e09680939b299244171ba6e0b2da8b723fae
Meindert Kempe <34239778+MeindertKempe@users.noreply.github.com>
2019-01-29 21:07:04 +0100

    Make database class a singleton

--- src/main/java/com/mk/zermelo/Database.java
+++ src/main/java/com/mk/zermelo/Database.java
@@ -1,9 +1,23 @@
 package com.mk.zermelo;
 
+import android.content.Context;
+
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @androidx.room.Database(version = 1, entities = {Appointment.class, User.class}, exportSchema = false)
 public abstract class Database extends RoomDatabase {
+
+	// Singleton pattern
+	private static Database instance;
+
+	public static synchronized Database getInstance(Context context) {
+		if(instance == null) {
+			instance = Room.databaseBuilder(context.getApplicationContext(), Database.class, "Database").fallbackToDestructiveMigration().build();
+		}
+		return  instance;
+	}
+
 	public abstract UserDao userDao();
 	public abstract AppointmentDao appointmentDao();
 }
