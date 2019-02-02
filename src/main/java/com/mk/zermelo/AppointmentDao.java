a8d3f33929ee15ddf620cce86ae634bc78b515ff
Meindert Kempe <34239778+MeindertKempe@users.noreply.github.com>
2019-02-02 23:03:23 +0100

    Improve Daos

--- src/main/java/com/mk/zermelo/AppointmentDao.java
+++ src/main/java/com/mk/zermelo/AppointmentDao.java
@@ -1,5 +1,8 @@
 package com.mk.zermelo;
 
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -8,8 +11,14 @@ import androidx.room.Query;
 public interface AppointmentDao {
 
 	@Insert
-	public void insertAppointment(Appointment appointment);
+	void insertAppointment(Appointment appointment);
 
-	@Query("DELETE FROM Appointment")
-	public void deleteAllAppointments();
+	@Query("DELETE FROM Appointment where userId = :userId")
+	void deleteAllAppointments(int userId);
+
+	// Get any appointments that start between the given timestamps.
+	@Query("SELECT * FROM appointment WHERE (userId = :userId) AND " +
+			"((start >= :timeStart AND start <= :timeEnd) OR " +
+			"([end] >= :timeStart AND [end] <= :timeEnd))")
+	LiveData<List<Appointment>> getAppointments(int userId, long timeStart, long timeEnd);
 }
--- src/main/java/com/mk/zermelo/UserDao.java
+++ src/main/java/com/mk/zermelo/UserDao.java
@@ -1,11 +1,15 @@
 package com.mk.zermelo;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
 
 @Dao
 public interface UserDao {
 	@Insert
-	public void insertUser(User user);
+	long insertUser(User user);
 
+	@Query("SELECT * FROM User WHERE id = :userId")
+	LiveData<User> getUser(int userId);
 }
